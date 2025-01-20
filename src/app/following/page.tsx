7a38b5686485a0e604a3cb761991455592debbe7
AryaShewde <aryashewde4@gmail.com>
2025-01-21 00:40:50 +0530

    first commit

--- src/app/following/page.tsx
+++ src/app/following/page.tsx
@@ -1,18 +1,18 @@
-// import Feed from '@/components/Feed'
-// import Share from '@/components/Share'
-// import Link from 'next/link'
+import Feed from '@/components/Feed'
+import Share from '@/components/Share'
+import Link from 'next/link'
 
-const page = () => {
+const PopularTags = () => {
     return (
         <div className="">
-            {/* <div className='px-4 pt-4 flex justify-around text-textGray font-bold border-b-[1px] border-borderGray'>
+            <div className='px-4 pt-4 flex justify-around text-textGray font-bold border-b-[1px] border-borderGray'>
                 <Link className="pb-3 flex items-center" href="/">For you</Link>
                 <Link className="pb-3 flex items-center text-white border-b-4 border-iconBlue" href="/following">Following</Link>
             </div>
             <Share />
-            <Feed /> */}
+            <Feed />
         </div>
     )
 }
 
-export default page
+export default PopularTags
