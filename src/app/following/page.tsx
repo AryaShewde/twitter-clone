71ab89859386c8f77186b9d27f985bac1173161c
AryaShewde <aryashewde4@gmail.com>
2025-01-22 00:34:54 +0530

    first commit

--- src/app/following/page.tsx
+++ src/app/following/page.tsx
@@ -1,5 +1,5 @@
-// import Feed from '@/components/Feed'
-// import Share from '@/components/Share'
+import Feed from '@/components/Feed'
+import Share from '@/components/Share'
 import Link from 'next/link'
 
 const PopularTags = () => {
@@ -9,8 +9,8 @@ const PopularTags = () => {
                 <Link className="pb-3 flex items-center" href="/">For you</Link>
                 <Link className="pb-3 flex items-center text-white border-b-4 border-iconBlue" href="/following">Following</Link>
             </div>
-            {/* <Share />
-            <Feed /> */}
+            <Share />
+            <Feed />
         </div>
     )
 }
