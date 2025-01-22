8598d8cf85e87b27e6f982425da471c8769cdfbc
AryaShewde <aryashewde4@gmail.com>
2025-01-23 00:50:27 +0530

    first commit

--- src/app/[username]/status/[postId]/page.tsx
+++ src/app/[username]/status/[postId]/page.tsx
@@ -1,30 +1,30 @@
-"use client"
-
 import Comments from "@/components/Comments";
 import Image from "@/components/Image";
-// import SinglePost from "@/components/SinglePost";
+import SinglePost from "@/components/SinglePost";
 import Link from "next/link";
 
-// const fetchFileIds = async (): Promise<{ fileData: { fileId: string; description: string }[] }> => {
-//   try {
-//     const response = await fetch(`${process.env.FETCH_URL}/api/getImageFileIds`, {
-//       cache: "no-store",
-//     });
-//     const data = await response.json();
-//     return data;
-//   } catch (error) {
-//     console.error("Error fetching file IDs:", error);
-//     return { fileData: [] };
-//   }
-// };
+const fetchFileIds = async (): Promise<{ fileData: { fileId: string; description: string }[] }> => {
+  try {
+    const response = await fetch(`${process.env.FETCH_URL}/api/getImageFileIds`, {
+      cache: "no-store",
+    });
+    const data = await response.json();
+    return data;
+  } catch (error) {
+    console.error("Error fetching file IDs:", error);
+    return { fileData: [] };
+  }
+};
+
+type Params = Promise<{ postId: string }>
 
 
-// const StatusPage = async ({ params }: { params: { postId: string } }) => {
-const StatusPage = ({ params }: { params: { postId: string } }) => {
-  const { postId } = params;
-  // const data = await fetchFileIds();
-  // const mainidarray = data.fileData;
-  // const foundFileDetail = mainidarray.find((file) => file.fileId === postId);
+const Page = async ({ params }: { params: Params }) => {
+  // const Page = () => {
+  const { postId } = await params;
+  const data = await fetchFileIds();
+  const mainidarray = data.fileData;
+  const foundFileDetail = mainidarray.find((file) => file.fileId === postId);
 
   return (
     <div className="">
@@ -33,12 +33,11 @@ const StatusPage = ({ params }: { params: { postId: string } }) => {
           <Image path="icons/back.svg" alt="back" w={24} h={24} />
         </Link>
         <h1 className="font-bold text-lg">Post</h1>
-        <h2>{postId}</h2>
       </div>
-      {/* <SinglePost id={postId} desc={foundFileDetail?.description || ""} type="status" /> */}
+      <SinglePost id={postId} desc={foundFileDetail?.description || ""} type="status" />
       <Comments />
     </div>
   );
 };
 
-export default StatusPage;
+export default Page;
