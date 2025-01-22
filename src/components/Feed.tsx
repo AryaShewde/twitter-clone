9778c5716f76d770ddf00e5d14f20b230f78eb0e
AryaShewde <aryashewde4@gmail.com>
2025-01-23 02:18:24 +0530

    first commit

--- src/components/Feed.tsx
+++ src/components/Feed.tsx
@@ -1,8 +1,5 @@
-"use client"
-import { useState, useEffect } from "react";
-import Post from "./Post";
+import Post from "./Post"
 
-// Function to fetch file IDs from your API
 const fetchFileIds = async (): Promise<{ fileData: { fileId: string; description: string }[] }> => {
   try {
     const response = await fetch(`https://twitter-clone-azure-eta.vercel.app/api/getImageFileIds`);
@@ -14,31 +11,18 @@ const fetchFileIds = async (): Promise<{ fileData: { fileId: string; description
   }
 };
 
-// Feed component (client-side only)
-const Feed = () => {
-  const [fileData, setFileData] = useState<{ fileId: string; description: string }[]>([]); // State to store file data
-
-  // Fetch data when the component mounts
-  useEffect(() => {
-    const fetchData = async () => {
-      const data = await fetchFileIds();
-      setFileData(data.fileData); // Set the fetched data into state
-    };
-    fetchData(); // Call fetchData once when the component is mounted
-  }, []); // Empty dependency array ensures it runs only once
-
-  // Render the posts
+const Feed = async () => {
+  const data = await fetchFileIds();
+  if (!data.fileData || data.fileData.length === 0) {
+    return <div className="text-center p-4">Data not found</div>;
+  }
   return (
-    <div>
-      {fileData.length === 0 ? (
-        <div className="text-center p-4">Data not found</div>
-      ) : (
-        fileData.slice().reverse().map((filesubdata) => (
-          <Post key={filesubdata.fileId} id={filesubdata.fileId} desc={filesubdata.description} />
-        ))
-      )}
+    <div className=''>
+      {data.fileData.slice().reverse().map((filesubdata) => (
+        <Post key={filesubdata.fileId} id={filesubdata.fileId} desc={filesubdata.description} />
+      ))}
     </div>
-  );
-};
+  )
+}
 
 export default Feed;
